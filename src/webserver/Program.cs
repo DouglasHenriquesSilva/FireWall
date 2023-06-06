b97f5020b279e6aa09b5ed0ecd491461e23e521c
Douglas Henriques Silva <130110097+DouglasHenriquesSilva@users.noreply.github.com>
2023-06-05 21:44:45 -0300

    Add files via upload

--- src/webserver/Program.cs
+++ src/webserver/Program.cs
@@ -12,28 +12,23 @@ app.MapGet("/firewall/{site}/{comando}", async context =>
     process.FileName = "sh";
     process.Arguments = $"/root/comandofw.sh {site} {comando}";
     process.RedirectStandardOutput = true;
+
     System.Diagnostics.Process cmd = System.Diagnostics.Process.Start(process);
     cmd.WaitForExit();
 
+    string output = await cmd.StandardOutput.ReadToEndAsync();
 
     await context.Response.WriteAsync("<!doctype html>");
-    await context.Response.WriteAsync("<html>");
-       
+    await context.Response.WriteAsync("<html lang='pt-br'>");
     await context.Response.WriteAsync("<head>");
-    //Definindo o título da página
+    await context.Response.WriteAsync("<meta charset='UTF-8'>");
+    // Definindo o título da página
     await context.Response.WriteAsync("<title>SITE LIBERADO OU BLOQUEADO</title>");
-    await context.Response.WriteAsync("</head>"); 
-       
-       
-       
+    await context.Response.WriteAsync("</head>");
     await context.Response.WriteAsync("<body>");
-
-
-   // Adicionando a tag <img> para exibir a imagem
-   await context.Response.WriteAsync("<img src=\"/root/webserver/igarape.jpg\">");
-
-   await context.Response.WriteAsync("<img src=\"/home/douglas/Imagens/igarape.jpg\" alt=\"A imagem nao aparece\">");
-
+    // Adicionando a tag <img> para exibir a imagem
+    await context.Response.WriteAsync("<img src=\"/root/webserver/igarape.jpg\">");
+    await context.Response.WriteAsync("<img src=\"/home/douglas/Imagens/igarape.jpg\" alt=\"A imagem não aparece\">");
 
     string status;
     if (comando == "DROP")
@@ -49,8 +44,18 @@ app.MapGet("/firewall/{site}/{comando}", async context =>
         status = "desconhecido";
     }
 
-    await context.Response.WriteAsync($"<h1> O SITE: <font color=\"red\"><strong>{site}</strong></font> FOI: <font color=\"red\"><strong>{status}</strong></font></h1>");
-  
+    await context.Response.WriteAsync($"<h1>O SITE: <font color=\"red\"><strong>{site}</strong></font> FOI: <font color=\"red\"><strong>{status}</strong></font></h1>");
+
+    await context.Response.WriteAsync("<h2>--------------------------------------------------------------------</h2>");
+    await context.Response.WriteAsync("<h1>Relatório de sites bloqueados:</h1>");
+
+    // Adicionando o comando para gerar o relatório de sites bloqueados
+    process.Arguments = "-c \"iptables-save | grep -E 'OUTPUT.*-m string.*--string' | awk '{print $12}'\"";
+    cmd = System.Diagnostics.Process.Start(process);
+    cmd.WaitForExit();
+    output = await cmd.StandardOutput.ReadToEndAsync();
+
+    await context.Response.WriteAsync("<pre>" + output + "</pre>");
 
     // Adicionando o JavaScript para retornar à página anterior após 5 segundos
     await context.Response.WriteAsync("<script>");
@@ -59,6 +64,7 @@ app.MapGet("/firewall/{site}/{comando}", async context =>
 
     await context.Response.WriteAsync("</body>");
     await context.Response.WriteAsync("</html>");
-    });
-    app.Run();
+});
+
+app.Run();
 
